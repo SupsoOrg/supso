2f66c2a3fad1047447f00eda142792712fbfe949
J. R. Pickhardt <pickhardt@gmail.com>
2016-07-03 13:23:04 -0700

    Update version to 0.9.6

--- lib/supso/version.rb
+++ lib/supso/version.rb
@@ -2,7 +2,7 @@ module Supso
   if !defined?(Supso::VERSION)
     VERSION_MAJOR = 0
     VERSION_MINOR = 9
-    VERSION_PATCH = 5
+    VERSION_PATCH = 6
     VERSION = [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH].join('.')
   end
 end
--- spec/supso_spec.rb
+++ spec/supso_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Supso do
   it 'has the right version' do
-    expect(Supso::VERSION).to eq('0.9.5')
+    expect(Supso::VERSION).to eq('0.9.6')
   end
 
   it 'has the right api endpoint' do
