7a28871c58f045340a61e55d4274c3a888bf3696
J. R. Pickhardt <pickhardt@gmail.com>
2017-04-14 21:44:45 -0700

    Update version to 0.10.1

--- spec/supso_spec.rb
+++ spec/supso_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Supso do
   it 'has the right version' do
-    expect(Supso::VERSION).to eq('0.10.0')
+    expect(Supso::VERSION).to eq('0.10.1')
   end
 
   it 'has the right api endpoint' do
